374ffc5f3fec8fc2171a1a94c5ff22ae9a8588d0
95-txt <sinanlw95@gmail.com>
2025-08-14 13:54:15 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <img
+        src="/src/assets/sinanface.JPG"
+        alt="face"
+        className="h-80 w-60 object-cover rounded-2xl"
+      />
+    </div>
+  );
+}
+
+export default Home;
